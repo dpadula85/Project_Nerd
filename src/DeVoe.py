Corrected the selection of dipoles and centers in the calculation of optical properties

src/DeVoe.py
@@ -362,13 +362,16 @@ if __name__ == '__main__':
         cd_freq = 0 
 
         for m in range(A.shape[0]):
+
             for n in range(m, A.shape[1]):
 
-                e_m = orientations[m]
-                e_n = orientations[n]
+                m_idx = m / 3
+                e_m = orientations[m_idx]
+                n_idx = n / 3
+                e_n = orientations[n_idx]
 
                 # Calculate ECD spectrum value
-                r_mn = centers[m] - centers[n]
+                r_mn = centers[m_idx] - centers[n_idx]
                 C_mn = np.dot(r_mn, np.cross(e_m, e_n)) # - 4 * bj * np.dot(e_m, e_nmag) 
 
                 # Calculate Absorption spectrum value
@@ -382,7 +385,7 @@ if __name__ == '__main__':
                 
         sys.exit()
 
-        # CHECK THE CONSTANT FOR UV
+        # CHECK THE CONSTANTS FOR UV and CD
         uv_freq = uv_freq * freq
         uv_system = np.r_[uv_system, uv_freq]
 

src/calcpol.py
@@ -34,7 +34,8 @@ CGS_to_Acube = 1e24
 # Prefactor for absorption spectra in CGS units 
 CGS_CNST = 3000 * log10 * CGS_h * CGS_c / (32 * np.pi**3 * N_A)
 CGS_CNST2 = 6909 / (8 * pi_sq * N_A)
-CGS_CNST3 = 24 * pi_sq * N_A / (3298 * CGS_c**4)
+CGS_CNST3 = 24 * pi_sq * N_A / (3298 * CGS_c**2)
+# CGS_CNST3 = 24 * pi_sq * N_A / (3298 * CGS_c**4)
 
 #
 # Lorentzian and Gaussian Bandwidth from Chirality, 2013, 25, 243